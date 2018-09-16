4952770fdbf00998558d8e12a6eba1fc34d8db9c
ljanas <lukasz.janas@soprasteria.com>
2018-09-16 12:34:45 +0200

    Initial commit

---
+++ test/pca_example.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+sns.set()
+
+rng = np.random.RandomState(1)
+X = np.dot(rng.rand(2, 2), rng.randn(2, 200)).T
+plt.scatter(X[:, 0], X[:, 1])
+plt.axis('equal')
