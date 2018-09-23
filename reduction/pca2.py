61e1dd147b9f2a76ff5096024bdbd5ff6b0d8585
WookiePL <wookiepl@outlook.com>
2018-09-23 22:11:30 +0200

    Show names of features after calculating variance

--- reduction/pca2.py
+++ reduction/pca2.py
@@ -1,20 +1,24 @@
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+
 
 def show_intrinsic_dimensions(url, title):
     # load dataset into Pandas DataFrame
-    df = pd.read_csv(url, names=['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal'])
+    df = pd.read_csv(url,
+                     names=['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak',
+                            'slope', 'ca', 'thal'])
 
-    features = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']
+    features = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca',
+                'thal']
     df = df.replace('?', '0')
     df.convert_objects(convert_numeric=True)
 
+    # normalize data
+    data_scaled = pd.DataFrame(preprocessing.scale(df), columns=df.columns)
 
     pca = PCA()
-
     pca.fit(df)
 
     features2 = range(pca.n_components_)
@@ -25,12 +29,18 @@ def show_intrinsic_dimensions(url, title):
     plt.xlabel('PCA feature')
     plt.show()
 
+    indexes = []
+    for i in range(13):
+        indexes.append('PC-' + (1 + i).__str__())
+
+    pd.set_option('display.max_columns', None)
+    print(pd.DataFrame(pca.components_, columns=data_scaled.columns, index=indexes))
+
 
 url1 = "D:\\mgr\\heart-disease\\processed.switzerland.data"
 url2 = "D:\\mgr\\heart-disease\\processed.cleveland.data"
 url3 = "D:\\mgr\\heart-disease\\processed.hungarian.data"
 url4 = "D:\\mgr\\heart-disease\\processed.va.data"
-
 
 show_intrinsic_dimensions(url1, 'Switzerland')
 show_intrinsic_dimensions(url2, 'Cleveland')
