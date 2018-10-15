1e730ffa6e619ecc5239f0872a6436e537e622bc
WookiePL <wookiepl@outlook.com>
2018-10-15 10:52:21 +0200

    improved pca for heart-disease

--- reduction/pca3_reduction.py
+++ reduction/pca3_reduction.py
@@ -1,49 +1,53 @@
 import matplotlib.pyplot as plt
-import pandas as pd
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
 import numpy as np
+import pandas as pd
 from matplotlib.colors import ListedColormap
+from sklearn import metrics
+from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-from reduction.utils import save_plot_as_png_file, standardise_classes
+from reduction.utils import save_plot_as_png_file, standardise_classes, plot_decision_regions
 
 
 def process_pca(url, title):
-    # load dataset into Pandas DataFrame
+    # załadowanie zbioru danych do Pandas DataFrame
     df = pd.read_csv(url,
                      names=['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak',
                             'slope', 'ca', 'thal', 'num, the predicted attribute'])
 
     df = df.replace('?', '0')
-    df.columns = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal', 'num, the predicted attribute']
+    df.columns = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope',
+                  'ca', 'thal', 'num, the predicted attribute']
     df.convert_objects(convert_numeric=True)
+    print(df.dtypes)
 
-    #podział na zbiór cech i klasy
+    # podział na zbiór cech i klasy
     X, y = df.iloc[:, :13].values, df.iloc[:, 13].values
 
+    #standaryzacja klas (wartosci > 1 => 1)
     y = standardise_classes(y)
 
-    #podział danych na 70% zbiór treningowy, 30% testowy
+    # podział danych na 70% zbiór treningowy, 30% testowy
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-    #standaryzacja danych
+    # standaryzacja danych
     sc = StandardScaler()
     X_train_std = sc.fit_transform(X_train)
     X_test_std = sc.transform(X_test)
 
-    #otrzymywanie wartości własnych (eigenvalues)
+    # otrzymywanie wartości własnych (eigenvalues)
     cov_mat = np.cov(X_train_std.T)
     eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
     print('Eigenvalues \n%s' % eigen_vals)
 
-    #suma wariancji
+    # suma wariancji
     total = sum(eigen_vals)
-    #explained variances
+    # explained variances
     var_exp = [(i / total) for i in sorted(eigen_vals, reverse=True)]
-    #cumulative sum of explained variances
-    cum_var_exp= np.cumsum(var_exp)
+    # cumulative sum of explained variances
+    cum_var_exp = np.cumsum(var_exp)
 
     plt.bar(range(1, 14), var_exp, alpha=0.5, align='center',
             label='individual explained variance')
@@ -55,14 +59,12 @@ def process_pca(url, title):
     plt.tight_layout()
     plt.show()
 
-    #transformacja cech
-
-
-    # Make a list of (eigenvalue, eigenvector) tuples
+    # transformacja cech
+    # make a list of (eigenvalue, eigenvector) tuples
     eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])
                    for i in range(len(eigen_vals))]
 
-    # Sort the (eigenvalue, eigenvector) tuples from high to low
+    # sort the (eigenvalue, eigenvector) tuples from high to low
     eigen_pairs.sort(key=lambda k: k[0], reverse=True)
 
     w = np.hstack((eigen_pairs[0][1][:, np.newaxis],
@@ -81,12 +83,10 @@ def process_pca(url, title):
     plt.ylabel('PC 2')
     plt.legend(loc='lower left')
     plt.tight_layout()
-    # plt.savefig('./figures/pca2.png', dpi=300)
+
     plt.show()
 
-
     X_train_std[0].dot(w)
-
 
     pca = PCA()
     X_train_pca = pca.fit_transform(X_train_std)
@@ -97,49 +97,19 @@ def process_pca(url, title):
     plt.xlabel('Principal components')
     plt.show()
 
-
     pca = PCA(n_components=2)
     X_train_pca = pca.fit_transform(X_train_std)
     X_test_pca = pca.transform(X_test_std)
-
 
     plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1])
     plt.xlabel('PC 1')
     plt.ylabel('PC 2')
     plt.show()
 
-    def plot_decision_regions(X, y, classifier, resolution=0.02):
-    # setup marker generator and color map
-        markers = ('s', 'x', 'o', '^', 'v')
-        colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-        cmap = ListedColormap(colors[:len(np.unique(y))])
-
-        # plot the decision surface
-        x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-        x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                               np.arange(x2_min, x2_max, resolution))
-        Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-        Z = Z.reshape(xx1.shape)
-        plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
-        plt.xlim(xx1.min(), xx1.max())
-        plt.ylim(xx2.min(), xx2.max())
-
-        # plot class samples
-        for idx, cl in enumerate(np.unique(y)):
-            plt.scatter(x=X[y == cl, 0],
-                        y=X[y == cl, 1],
-                        alpha=0.6,
-                        c=cmap(idx),
-                        edgecolor='black',
-                        marker=markers[idx],
-                        label=cl)
-
     lr = LogisticRegression()
     lr = lr.fit(X_train_pca, y_train)
 
-
-    plot_decision_regions(X_train_pca, y_train, classifier=lr)
+    plot_decision_regions(X_train_pca, y_train, classifier=lr, name="%s training" % title)
     plt.xlabel('PC 1')
     plt.ylabel('PC 2')
     plt.title(title + ', 2 component PCA, zbiór treningowy')
@@ -148,7 +118,7 @@ def process_pca(url, title):
     save_plot_as_png_file(plt)
     plt.show()
 
-    plot_decision_regions(X_test_pca, y_test, classifier=lr)
+    plot_decision_regions(X_test_pca, y_test, classifier=lr, name="%s test" % title)
     plt.xlabel('PC 1')
     plt.ylabel('PC 2')
     plt.title(title + ', 2 component PCA, zbiór testowy')
@@ -159,13 +129,12 @@ def process_pca(url, title):
     pass
 
 
-
 url1 = "D:\\mgr\\heart-disease\\processed.switzerland.data"
 url2 = "D:\\mgr\\heart-disease\\processed.cleveland.data"
 url3 = "D:\\mgr\\heart-disease\\processed.hungarian.data"
 url4 = "D:\\mgr\\heart-disease\\processed.va.data"
 
-#process_pca(url1, 'Switzerland')
+process_pca(url1, 'Switzerland')
 process_pca(url2, 'Cleveland')
-#process_pca(url3, 'Hungarian')
-#process_pca(url4, 'Long Beach, CA')
+process_pca(url3, 'Hungarian')
+process_pca(url4, 'Long Beach, CA')
